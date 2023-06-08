ac78e8fc590fa5a1f6986ff8e63d56e1370a63f1
Aakarsh Sinha <aakarshsak@gmail.com>
2023-06-08 18:25:47 +0530

    Initializing the repository

---
+++ src/main/java/com/sinha/onepunchman/OnePunchManApplication.java
@@ -0,0 +1,13 @@
+package com.sinha.onepunchman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePunchManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnePunchManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinha/onepunchman/OnePunchManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinha.onepunchman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePunchManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
